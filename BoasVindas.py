30f5012228b81051e7ca57cafa956d7af4ae0327
Álex Junio <alexshox@gmail.com>
2019-07-09 06:18:21 -0300

    No início era tudo mato
    No início era tudo mato

---
+++ BoasVindas.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+nome = input ("Qual o seu nome ?")
+dia = input ("Qula dia do seu nascimento?")
+mes = input ("Qual o mês do seu nascimento?")
+ano = input ("Qual seu ano de nascimento?")
+
+print("Bem vindo(a)",nome , "!")
---
+++ Variaveis.py
@@ -0,0 +1,7 @@
+
+
+nome = input ("Qual seu nome ?")
+idade = input ("Qual a sua idade?")
+peso =  input ("Qual seu peso?")
+
+print(nome , idade, peso)
