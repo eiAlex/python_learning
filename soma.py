b9d58b03d94733ae53a6702810755fbc9b4f180c
Álex Junio <alexshox@gmail.com>
2019-07-10 06:20:18 -0300

    Update soma.py

--- soma.py
+++ soma.py
@@ -1,8 +1,11 @@
 
-num1 = input ("Primeiro número ")
-num2 = input ("Segundo número ")
+num1 = int (input ("Primeiro número "))
+num2 = int (input ("Segundo número "))
 
 #conversão de string para inteiros
-soma = int (num1) + int (num2)
+#soma = int (num1) + int (num2)
 
-print ("Resultado: ", num1, " + ", num2, " = ", soma)
+soma =  num1 + num2
+
+print ("Resultado: ", num1, " + ", num2, " = ", soma) # fromato antigo
+print ('Resultado: {0} + {1} = {2} '.format(num1, num2, soma)) # argumentos em um vetor
