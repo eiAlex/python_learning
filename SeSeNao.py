639a86e5ea1547950562c51595478501dd7d7109
Álex Junio <alexshox@gmail.com>
2019-07-11 06:06:07 -0300

    commit

---
+++ SeSeNao.py
@@ -0,0 +1,12 @@
+   
+num1 = int (input("Informe o primeiro número: "))
+num2 = int (input("Informe o segundo número: "))
+
+if num1 > num2:
+    print("O {0} é maior que {1}".format(num1,num2))
+elif num2 > num1: 
+    print("O {0} é maior que {1}".format(num2,num1))
+else:
+    print("Números iguais")
+
+
