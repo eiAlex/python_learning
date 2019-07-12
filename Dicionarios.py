44148f5577f8cb4ccff8559aa63c508a8fa49b30
Álex Junio <alexshox@gmail.com>
2019-07-11 23:56:34 -0300

    commit

---
+++ Dicionarios.py
@@ -0,0 +1,7 @@
+# -*- codingÇ utf-8 -*- 
+
+dic = {1:'Janeiro', 2: 'Fevereiro', 3: 'Março'}
+
+print(len(dic))
+
+print(dic.)
---
+++ SequenciaStrigs.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*- 
+nome = "álex"
+print(len(nome)) # printa o tamanho da varíavel 
+
+print(nome.capitalize()) #primeira letra maiúscula
+
+print(nome.upper())
+
+print(nome.lower())
+
+print (nome.encode())
+
+
+print(nome.isalnum())
+
+print(nome.isalpha())
+
+print(nome.isnumeric())
+
