35720e4f9268ef68e9f923c0de2afccc8365df49
Álex Junio <alexshox@gmail.com>
2019-07-16 05:57:57 -0300

    Update bibilhotecas.py

--- bibilhotecas.py
+++ bibilhotecas.py
@@ -9,12 +9,24 @@ print(math.log(x))
 print(math.cos(x))
 
 
-from tkinter import *
-box = Button (texto = 'Ok', comand = 'exit')
-box.pack()
-box.mainloop()
+import smtplib
+server = smtplib.SMTP('mail.xx.net')
 
+# Envio de e-mail da conta @xy para a conta @xx
+server.sendmail 
+('leonardo@xy.com', 'leonardo@xx.net',
 
+"""To: leonardo@xx.net
+
+From: leonardo@xy.com
+
+Subject: Teste de envio de email
+
+Estou lhe enviando este email como um teste.
+
+""")
+server.quit()
+print('Envio ok')
 
 import time
 
