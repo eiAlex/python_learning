4eeaa5ad142c32d27f79f2ad57004174a5e4591b
Álex Junio <alexshox@gmail.com>
2019-07-16 05:38:58 -0300

    Update bibilhotecas.py

--- bibilhotecas.py
+++ bibilhotecas.py
@@ -4,6 +4,16 @@
 import math 
 x = 5
 print(math.sqrt(5))
+print(math.sin(x))
+print(math.log(x))
+print(math.cos(x))
+
+
+from tkinter import *
+box = Button (texto = 'Ok', comand = 'exit')
+box.pack()
+box.mainloop()
+
 
 
 import time
@@ -11,3 +21,4 @@ import time
 for i in range(1,6):
     time.sleep(1)
     print( f'Se passou {i} segundo(s).')
+
