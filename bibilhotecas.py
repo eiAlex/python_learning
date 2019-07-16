a1ab2ca326850c4a59d2ca07c1cbfc1fb1dfbe81
Álex Junio <alexshox@gmail.com>
2019-07-16 06:02:30 -0300

    Update bibilhotecas.py

--- bibilhotecas.py
+++ bibilhotecas.py
@@ -9,28 +9,34 @@ print(math.log(x))
 print(math.cos(x))
 
 
-import smtplib
-server = smtplib.SMTP('mail.xx.net')
+    import smtplib
+    server = smtplib.SMTP('mail.xx.net')
 
-# Envio de e-mail da conta @xy para a conta @xx
-server.sendmail 
-('leonardo@xy.com', 'leonardo@xx.net',
+    # Envio de e-mail da conta @xy para a conta @xx
+    server.sendmail 
+    ('leonardo@xy.com', 'leonardo@xx.net',
 
-"""To: leonardo@xx.net
+    """To: leonardo@xx.net
 
-From: leonardo@xy.com
+    From: leonardo@xy.com
 
-Subject: Teste de envio de email
+    Subject: Teste de envio de email
 
-Estou lhe enviando este email como um teste.
+    Estou lhe enviando este email como um teste.
 
-""")
-server.quit()
-print('Envio ok')
+    """)
+    server.quit()
+    print('Envio ok')
+
+
+
+ import random randint as range
+num = r (0,10)
+print(f ' O rando eh {num}')
+
 
 import time
 
 for i in range(1,6):
     time.sleep(1)
     print( f'Se passou {i} segundo(s).')
-
