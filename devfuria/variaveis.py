d0cb34777bf4486ffcaa81803cd17b1d51198a7c
Álex Junio <alexshox@gmail.com>
2019-07-26 06:08:32 -0300

    Create variaveis.py

---
+++ devfuria/variaveis.py
@@ -0,0 +1,22 @@
+# -*- coding utf-8 -*- 
+
+# variáveis 
+# Uma variável não pode ser utilizada em uma expressão sem ter sido inicializada
+# Não existe criação de variáveis automatica 
+
+# Este bloco mostra uma variável sendo instanciada e recebendo atribuição 
+
+real = 1.00
+euro = 4.21
+dolar = 3.78
+
+#podemos iniciar variáve tambem na mesma linha 
+
+pesosArgentinos, pesosUruguaio = 0.22 , 0.11
+
+# Variáveis tipos (null)/vazias são estanciadas com o None
+
+taxa = None
+
+print(taxa)
+
