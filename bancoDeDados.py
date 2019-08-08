7e829c5614e66bb72d6905be33c24d8da666ae33
Álex Junio <alexshox@gmail.com>
2019-08-08 06:09:51 -0300

    Update bancoDeDados.py

--- bancoDeDados.py
+++ bancoDeDados.py
@@ -42,4 +42,50 @@ print("------------------------------------------------")
 print(testeCursor.column_names)
 for table in dataBase:
   print(table)
- 
+
+#Cria DB se ele existe 
+testeCursor.execute("CREATE DATABASE IF NOT EXISTS DBTESTE;")
+
+#Cria a tabela 
+testeCursor.execute("CREATE TABLE IF NOT EXISTS DBTESTE.VENDEDORES (ID INT AUTO_INCREMENT PRIMARY KEY, NOME VARCHAR (255), ENDERECO VARCHAR(255))")
+
+#Vmostra a tabela 
+testeCursor.execute("SHOW TABLES FROM DBTESTE")
+
+for tabelas in testeCursor:
+  print(tabelas)
+
+#Inserção de dados no curso é passda por parametros
+
+sql = "INSERT INTO DBTESTE.VENDEDORES (NOME,ENDERECO) VALUES (%s, %s)"
+val = ("John" , "Higway 2100")
+
+testeCursor.execute(sql,val)
+
+#COMMITE DA TRANSAÇÃO 
+mydb.commit() 
+
+#printe de contagem de linhas
+print("Contagem de linhas", testeCursor.rowcount)
+
+#inserção de varias linhas 
+val = [
+  ("John" , "Higway 2100"),
+  ("Dereck" , "level center 13"),
+  ("Marie" , "lives town 12"),
+  ("John" , "Higway 2100"),
+  ("John" , "Higway 2100"),
+  ("John" , "Higway 2100"),
+  ("John" , "Higway 2100"),
+  ("John" , "Higway 2100"),
+  ("John" , "Higway 2100")
+]
+
+#para mais linhas execute many 
+testeCursor.executemany(sql,val) # para
+mydb.commit()
+
+#printe de contagem de linhas inteiradas
+print("Contagem de linhas", testeCursor.rowcount)
+
+mydb.close()
