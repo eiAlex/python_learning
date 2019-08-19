c72cf24c3c5cfd80681b4852c5eb5da3521fb17d
Álex Junio <alexshox@gmail.com>
2019-08-19 06:04:39 -0300

    Create funcoes.py

---
+++ devfuria/funcoes.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*- 
+
+# a palavra reservada  "def" define uma função utilizando da palavra "pass" podedmos cria uma função inutil
+
+def funcao():
+    pass
+
+# para criar um função com retorno 
+
+def funcao2():
+    return "Teste função"
+
+funcao2()
