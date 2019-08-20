712ba250393153856cb38141299b28bc5dc9f561
Álex Junio <alexshox@gmail.com>
2019-08-20 05:45:25 -0300

    Update Dicionarios.py

--- devfuria/Dicionarios.py
+++ devfuria/Dicionarios.py
@@ -3,16 +3,22 @@
 #Dicionários são coleçõe, com mapeamento nativo do python A associação destes dicionários 
 # é chave valor em que a chave é uma elemento imutável
 
-#criando um dicionário 
+#criando um dicionário, foi sobreescrevido
 pessoa = {
+    
+    "ID" : 1,
     "nome" : "João",
     "idade" : 39,
     "linguagem" : "PT-BR",
     
+    "ID" : 2,
     "nome" : "Maria",
     "idade" : 23,
     "linguagem" : "PT-PG"
 }
 
 #imprimindo o dicionário
-print(pessoa.value())
+print(pessoa["ID"])
+
+#tamanho do diciário 
+print(len(pessoa))
