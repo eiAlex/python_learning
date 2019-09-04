511f3384c6202f98300a3f98f49487416e1a08c5
Álex Junio <alexshox@gmail.com>
2019-09-04 13:37:50 -0300

    commit

--- pythonOrgBr/olaMundo.py
+++ pythonOrgBr/olaMundo.py
@@ -1,4 +1,27 @@
 # -*- encoding: utf-8 -*- 
-''' Seguindo a lista de exercicios do python.org.br'''
 
-print("Ola Mundo!")
+
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_M,
+    box_size=15,
+    border=1,
+)
+qr.add_data('''BEGIN:VCARD
+VERSION:3.0
+FN;CHARSET=UTF-8:Pedro Canalini
+N;CHARSET=UTF-8:;Pedro Canalini;;;
+EMAIL;CHARSET=UTF-8;type=WORK,INTERNET:pedroc@montreal.com.br
+TEL;TYPE=CELL:21975395588 
+TEL;TYPE=WORK,VOICE:2122764093 
+TITLE;CHARSET=UTF-8:Gerente Comercial | Diretoria Regional RJ
+ORG;CHARSET=UTF-8: Montreal Informática
+URL;type=WORK;CHARSET=UTF-8:https://www.montreal.com.br/
+END:VCARD
+''')
+qr.make(fit=True)
+
+img = qr.make_image(fill_color="#7e171d", back_color = "#dddddc")
+img.show()
+img.save('QrcodePedro_1.png', 'PNG')
