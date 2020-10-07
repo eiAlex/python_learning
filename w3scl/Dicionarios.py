af41a881620d70564d8b308f6fde24b838f074b1
Álex Junio <alexshox@gmail.com>
2020-10-06 23:22:07 -0300

    Update Dicionarios.py

--- w3scl/Dicionarios.py
+++ w3scl/Dicionarios.py
@@ -15,3 +15,27 @@ dic1 = {4:'Abril', 5: 'Maio'}
 dic.update(dic1)
 
 print("Valores: %s" %dic)
+
+for i in dic:
+    if dic[i] == dic[5]:
+        dic1 = {6:'junho', 7: 'Julho'}
+
+print(dic1)
+
+dict_cadastro =  {"joao":18,"maria":15, "Alex":15}
+
+contatos_lista = {('Yan', '1234-5678'), ('Pedro', '9999-9999'),
+                    ('Ana', '8765-4321'), ('Marina', '8877-7788')}
+
+dict_lista = dict(contatos_lista)
+
+
+print(type(dict_lista))
+
+print(dict_lista)
+
+for i in dict_lista:
+    if dict_lista[i] == dict_lista['Ana']:
+        print(i)
+    else:
+        print(1)
